0b329178125b776b5825afa0f6e9bc73c231c180
Finn <finn.merton@pwc.com>
2022-11-04 12:24:58 +0000

    differenceFromAnswer function working

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function generateRandomNumber(){
   let rnd = Math.floor(Math.random() * 11);
   return rnd;
 }
+
+function differenceFromAnswer(guess, answer){
+  if (guess < answer) {
+    return "Too low";
+  } else if (guess > answer) {
+    return "Too high";
+  } else {
+    return "Correct";
+  }
+}
